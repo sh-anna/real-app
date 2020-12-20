86c09685ab6b7c87146c1f3026f7dbb3e2d19bcd
sh-anna <62933455+sh-anna@users.noreply.github.com>
2020-12-20 16:11:30 +0200

    Add files via upload

---
+++ src/services/userService.js
@@ -0,0 +1,37 @@
+import http from "./httpService";
+import { apiUrl } from "../config.json";
+import jwtDecode from 'jwt-decode';
+
+const tokenKey = "token";
+
+export function logout(){
+    localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser(){
+
+try {
+
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+
+} catch(ex){
+
+    return null;
+
+}
+}
+
+export async function login(email, password){
+  const { data } = await http.post(`${apiUrl}/auth`, { email , password } );
+  localStorage.setItem(tokenKey, data.token);
+}
+
+
+const user = {
+  login,
+  getCurrentUser,
+  logout
+};
+
+export default user;
