25d6dfc026db4f9228948ab8c210ff7b44335c2e
sh-anna <62933455+sh-anna@users.noreply.github.com>
2020-12-20 16:10:06 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./App.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Home from "./components/home";
 import About from "./components/about";
-import Signup from "./components/signup"
-import { Switch, Route } from "react-router-dom"; //rrd
+import Signup from "./components/signup";
+import Signin from "./components/signin";
+import Logout from "./components/logout";
+import { Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import userService from "./services/userService";
 
- 
-function App() {
+class  App extends Component {
+
+  state = {};
+
+  componentDidMount(){
+    const user = userService.getCurrentUser();
+    this.setState({ user });
+  }
+
+  render(){
+
+    const { user } = this.state;
+
   return (
     <React.Fragment>
       <header>
-        <Navbar />
+        <ToastContainer />
+        <Navbar user={user}/>
       </header>
-      <main className="minh-900">
+      <main style={{ minHeight: 900 }}>
         <Switch>
-          <Route path="/signup" component={Signup}/>
+          <Route path="/logout" component={Logout} /> 
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
           <Route path="/about" component={About} />
           <Route path="/" exact component={Home} />
         </Switch>
       </main>
-      <footer >
-          <Footer />
+      <footer>
+        <Footer />
       </footer>
-      
     </React.Fragment>
   );
 }
- 
+
+}
+
 export default App;
