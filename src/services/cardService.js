cea63ee777abc0cd79f3a67afa738b52b8807d7d
sh-anna <62933455+sh-anna@users.noreply.github.com>
2020-12-30 22:42:43 +0200

    Add files via upload

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -1,13 +1,34 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
+export function deleteCard(cardId){
+  return http.delete(`${apiUrl}/cards/${cardId}`);
+}
+
+export function getCard(cardId){
+  return http.get(`${apiUrl}/cards/${cardId}`);
+}
+
+export function editCard(card){
+  const cardId = card._id;
+  delete card._id;
+  return http.put(`${apiUrl}/cards/${cardId}`, card);
+}
+
+export function getMyCards(){
+  return http.get(`${apiUrl}/cards/my-cards`);
+}
 
 export function createCard(card){
-    return http.post(`${apiUrl}/cards`, card);
+  return http.post(`${apiUrl}/cards`, card);
 }
 
 const service = {
-    createCard
+  createCard,
+  getMyCards,
+  editCard,
+  getCard,
+  deleteCard
 };
 
 export default service;
